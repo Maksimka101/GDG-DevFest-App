abb9e06168276397afb7f1a2108a47bf312e6bfb
Maksimka101 <zemlyanikin.max@yandex.ru>
2019-10-26 17:25:54 +0300

    Remove unused code.

--- test/api_test.dart
+++ test/api_test.dart
@@ -1,6 +1,6 @@
-import 'package:flutter_devfest/home/home_provider.dart';
-import 'package:flutter_devfest/network/index.dart';
-import 'package:flutter_test/flutter_test.dart';
+//import 'package:flutter_devfest/home/home_provider.dart';
+//import 'package:flutter_devfest/network/index.dart';
+//import 'package:flutter_test/flutter_test.dart';
 
 void main() {
 //  group(("Api tests"), () {});
